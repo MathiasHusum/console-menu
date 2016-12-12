00111a739414142b6d68516f4b91a709e6ec6325
soipi <soipi@DESKTOP-8M1B4KG>
2016-12-12 10:03:57 +0100

    Add project files.

---
+++ console menu/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_menu
+{
+    public delegate void MenuValgt();
+    public class Menu
+    {
+        
+        public List<MenuItem> menu = new List<MenuItem>();
+        public void AddItem(MenuItem m)
+        {
+            menu.Add(m);
+        }
+       
+    }
+    public class MenuItem
+    {
+        public string navn;
+        public MenuValgt mv;
+    }
+}
---
+++ console menu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_menu
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.run();
+
+        }
+
+        public void run()
+        {
+            Menu menu = new Menu();
+            menu.AddItem(new MenuItem() { navn = "opret", mv = new MenuValgt(opret) });
+            menu.AddItem(new MenuItem() { navn = "rediger", mv = new MenuValgt(EditItem) });
+            menu.AddItem(new MenuItem() { navn = "slet", mv = new MenuValgt(DeleteItem) });
+
+            menu.menu[Convert.ToInt32( Console.ReadLine())].mv();
+            Console.ReadLine();
+        }
+
+        public void opret()
+        {
+            Console.WriteLine("opret");
+        }
+        public void EditItem()
+        {
+            Console.WriteLine("rediger");
+        }
+        public void DeleteItem()
+        {
+            Console.WriteLine("slet");
+        }
+    }
+}
